9745038f73743bbed7fb2dc5598afafa54f74bef
abebe <rebuildaby@gmail.com>
2024-05-02 18:05:51 +0900

    v-if,v-show,v-bind

--- Vue.js入門/app.js
+++ Vue.js入門/app.js
@@ -1,7 +1,7 @@
 var items = [
   {
     name: "万年筆",
-    price: 300,
+    price: 100,
     quantity: 0,
   },
   {
@@ -20,6 +20,34 @@ var vm = new Vue({
   el: "#app",
   data: {
     items: items,
+  },
+  filters: {
+    numberWithDelimiter: function (value) {
+      if (!value) {
+        return "0";
+      }
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+  },
+  computed: {
+    //算出プロパティ
+    totalPrice: function () {
+      return this.items.reduce(function (sum, item) {
+        return sum + item.price * item.quantity;
+      }, 0);
+    },
+    totalPriceWithTax: function () {
+      return Math.floor(this.totalPrice * 1.08);
+    },
+    canBuy: function () {
+      return this.totalPrice >= 1000; //1000円以上から購入可能にする
+    },
+    errorMessageStyle: function () {
+      return {
+        border: this.canBuy ? "" : "1px solid red",
+        color: this.canBuy ? "" : "red",
+      };
+    },
   },
 });
 
